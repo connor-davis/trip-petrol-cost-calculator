b4331be65c903c54d1e7d66559baf953a431ebf8
Connor Davis <19422@kloofhigh.co.za>
2022-03-25 11:20:24 +0200

    Icons and pwa

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,21 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import { registerSW } from "virtual:pwa-register";
 
 import "./index.css";
 import App from "./App";
 
+if (typeof global === "undefined") {
+  window.global = window;
+}
+
 render(() => <App />, document.getElementById("root"));
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
+
+if (typeof window !== "undefined") {
+  import("./sw");
+}
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,90 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  resolve: {
+    alias: {
+      "@src": resolve(__dirname, "src"),
+    },
+  },
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      srcDir: "src",
+      base: "/",
+      workbox: {
+        cleanupOutdatedCaches: false,
+        sourcemap: true,
+      },
+      id: "/",
+      includeAssets: [
+        "./src/robots.txt",
+        "./src/assets/tripcost-x16.png",
+        "./src/assets/tripcost-x32.png",
+        "./src/assets/tripcost-x64.png",
+        "./src/assets/tripcost-x128.png",
+        "./src/assets/tripcost-x144.png",
+        "./src/assets/tripcost-x256.png",
+        "./src/assets/tripcost-x512.png",
+      ],
+      strategies: "injectManifest",
+      manifest: {
+        short_name: "TPCC",
+        name: "Trip Petrol Cost Calculator",
+        icons: [
+          {
+            src: "assets/tripcost-x16.png",
+            sizes: "16x16",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "assets/tripcost-x32.png",
+            sizes: "32x32",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "assets/tripcost-x64.png",
+            sizes: "64x64",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "assets/tripcost-x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "assets/tripcost-x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "assets/tripcost-x256.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "assets/tripcost-x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+    }),
+  ],
   build: {
-    target: 'esnext',
+    target: "esnext",
     polyfillDynamicImport: false,
   },
 });
